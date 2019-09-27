29c581301b5334f4c058dbb5d57727c7e83e6312
iosprogee <iosprogee@gmail.com>
2019-09-27 08:43:57 +0800

    Updates

---
+++ precache-manifest.f335e313c220b5dcf3e3952ba069cbdc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0e6f92b821bff0c4bd8",
+    "url": "/gh-react/static/css/main.2cc3c575.chunk.css"
+  },
+  {
+    "revision": "d0e6f92b821bff0c4bd8",
+    "url": "/gh-react/static/js/main.d0e6f92b.chunk.js"
+  },
+  {
+    "revision": "6e4bf57170e88701cc95",
+    "url": "/gh-react/static/css/1.78896a3e.chunk.css"
+  },
+  {
+    "revision": "6e4bf57170e88701cc95",
+    "url": "/gh-react/static/js/1.6e4bf571.chunk.js"
+  },
+  {
+    "revision": "00fa094d77cfe27a3603",
+    "url": "/gh-react/static/js/runtime~main.00fa094d.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/gh-react/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/gh-react/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/gh-react/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/gh-react/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/gh-react/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "170dd130df5e68728724aa6c2075aa5d",
+    "url": "/gh-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c71e0872d71eb48f9b36c72990ffefda.js"
+  "/gh-react/precache-manifest.f335e313c220b5dcf3e3952ba069cbdc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/gh-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
